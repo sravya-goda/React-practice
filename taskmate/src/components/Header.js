b1357db23062071cfa3cc27ad40e167ccaea4715
sravya-goda <sravya.maddu@godainfotech.in>
2025-07-30 15:48:31 -0700

    Add React project files without submodule

---
+++ taskmate/src/components/Header.js
@@ -0,0 +1,10 @@
+import Logo from "../assests/logo.png"
+
+export const Header = () => {
+  return (
+    <div>
+        <img className="logo" src={Logo} alt=""/>
+        {/* <h1>Sravyaaa</h1> */}
+    </div>
+  )
+}
