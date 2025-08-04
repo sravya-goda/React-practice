495b8f3542c334bdcd71d4ef183b4fd5658f1cc2
sravya-goda <sravya.maddu@godainfotech.in>
2025-08-04 16:40:18 -0700

    undefined values

--- taskmate/src/components/ShowTask.js
+++ taskmate/src/components/ShowTask.js
@@ -1,30 +1,36 @@
-export const ShowTask = () => {
-    const tasks = [
-        {id: 10001, name: "TASK A", time: "2:09:01 AM 9/14/2030"},
-        {id: 10002, name: "TASK B", time: "2:09:01 AM 9/14/2030"},
-        {id: 10003, name: "TASK C", time: "2:09:01 AM 9/14/2030"},
-    ]
-    return(
-        <section className="ShowTask">
-            <div className="head">
-                <div>
-                    <span className="title">Todo</span>
-                    <span className="count">0</span>
-                </div>
-                <button className="clearAll">Clear All</button>
+export const ShowTask = ({tasklist, setTasklist, task, setTask}) => {
+
+    const handleEdit = (id) => {
+        const seletedTask = tasklist.find(todo => todo.id === id);
+        setTask(seletedTask);
+    }
+
+    const handleDelete = (id) => {
+        const updatedTasklist = tasklist.filter(todo => todo.id !== id);
+        setTasklist(updatedTasklist);
+    }
+
+  return (
+    <section className="showTask">
+        <div className="head">
+            <div>
+                <span className="title">Todo</span>
+                <span className="count">{tasklist.length}</span>
             </div>
-            <ul>
-                {tasks.map((task) => (
-                    <li>
-                        <p>
-                            <span className="name">{task.name}</span>
-                            <span className="time">{task.time}</span>
-                        </p>
-                        <i className="bi bi-pencil-square"></i>
-                        <i className="bi bi-trash"></i>
-                    </li>
-                ))}
-            </ul>
-        </section>
-    )
+            <button onClick={() => setTasklist([])} className="clearAll">Clear All</button>
+        </div>
+        <ul>
+            { tasklist.map((todo) => (
+                <li key={todo.id}>
+                    <p>
+                        <span className="name">{todo.name}</span>
+                        <span className="time">{todo.time}</span>
+                    </p>
+                    <i onClick={() => handleEdit(todo.id)} className="bi bi-pencil-square"></i>
+                    <i onClick={() => handleDelete(todo.id)} className="bi bi-trash"></i>
+                </li>
+            )) }            
+        </ul>
+    </section>
+  )
 }
